7b2ad9c59f09dc67afa95e46e8189be2875ebb64
JosephQuayle <josephquayle@hotmail.com>
2023-10-15 14:46:12 +0100

    fixed toggle issue

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const hamBtn = document.getElementById("hamBtn");
 const mobNav = document.getElementById("mobNav");
 
 hamBtn.addEventListener("click", toggleNav);
+mobNav.style.display = "none";
 
 function toggleNav() {
-  if (mobNav.style.display === "none") {
-    mobNav.style.display = ""; // This will use the value from the CSS, which is "none"
+  if (mobNav.style.display === "") {
+    mobNav.style.display = "none"; // This will use the value from the CSS, which is "none"
   } else {
-    mobNav.style.display = "none";
+    mobNav.style.display = "";
   }
 }
